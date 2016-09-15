8c3100a6fd06b8dffef6ec3a01424e90f5c27307
Nina Claudine Ramos <ninaclaudine@gmail.com>
2016-09-15 08:14:12 +0800

    Adds first regex

--- RegexMatch/src/regexmatch/RegexMatch.java
+++ RegexMatch/src/regexmatch/RegexMatch.java
@@ -2,9 +2,14 @@
 package regexmatch;
 
 public class RegexMatch {
-
+    
+    public static boolean isTrue(String str){
+        return str.matches("[tT]rue");
+    }
+    
+    
     public static void main(String[] args) {
-      
+        System.out.println(isTrue("true"));
     }
     
 }
