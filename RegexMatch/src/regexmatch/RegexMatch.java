270080ae36c15ce2fffdc8f98b73bd5b0b3f8e69
Nina Claudine Ramos <ninaclaudine@gmail.com>
2016-09-15 08:21:45 +0800

    Adds another regex

--- RegexMatch/src/regexmatch/RegexMatch.java
+++ RegexMatch/src/regexmatch/RegexMatch.java
@@ -7,9 +7,13 @@ public class RegexMatch {
         return str.matches("[tT]rue");
     }
     
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
+    }
     
     public static void main(String[] args) {
         System.out.println(isTrue("true"));
+        System.out.println(containsFalse("gasdhgsadghsg"));
     }
     
 }
